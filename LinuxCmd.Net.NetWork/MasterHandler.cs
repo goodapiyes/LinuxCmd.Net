25b91d01caa37b9abeadc61e36556826086e49be
xuwei <2098240977@qq.com>
2019-08-12 17:09:46 +0800

    提交

--- LinuxCmd.Net.NetWork/MasterHandler.cs
+++ LinuxCmd.Net.NetWork/MasterHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -13,10 +14,12 @@ namespace LinuxCmd.Net.NetWork
         public string Ip { get; private set; }
         public int? Port { get; private set; }
         public TcpSocketServer Server;
+        public ConcurrentDictionary<string, TcpSocketSession> Sessions { get; private set; }
         public MasterHandler()
         {
             this.Ip = ConfigHander.GetString("local:ip");
             this.Port = ConfigHander.GetInt("local:port");
+            Sessions=new ConcurrentDictionary<string, TcpSocketSession>();
         }
 
         public void Listening()
@@ -33,17 +36,41 @@ namespace LinuxCmd.Net.NetWork
             Server.ClientDisconnected += ClientDisconnected;
             Server.ClientDataReceived += ClientDataReceived;
             Server.Listen();
-            
             LogHelper.Logger.Information("Server Listening...");
+        }
+
+        public bool Issue(string ip,string cmd)
+        {
+            try
+            {
+                foreach (var session in Sessions)
+                {
+                    if(session.Key.Contains(ip))
+                        Server.SendTo(session.Value.SessionKey,Encoding.UTF8.GetBytes(cmd));
+                }
+            }
+            catch (Exception e)
+            {
+                LogHelper.Logger.Error($"Issue {ip},Error:{e.Message},{e.StackTrace}");
+                return false;
+            }
+            return true;
         }
 
         private void ClientConnected(object sender, TcpClientConnectedEventArgs e)
         {
+            
+            var key = e.Session.RemoteEndPoint.ToString();
+            Sessions.AddOrUpdate(key, e.Session, (k, o) => o);
             Console.WriteLine($"客户端 {e.Session.RemoteEndPoint} 已连接 {e.Session}.");
         }
 
         private void ClientDisconnected(object sender, TcpClientDisconnectedEventArgs e)
         {
+            var key = e.Session.RemoteEndPoint.ToString();
+            TcpSocketSession removeSession;
+            if(Sessions.ContainsKey(key))
+                Sessions.TryRemove(key, out removeSession);
             Console.WriteLine($"客户端 {e.Session} 关闭了连接.");
         }
 
